56f5284c210304b6f7ed3bdcd09d9336a34393fa
= <=>
2025-03-01 16:00:27 -0400

    [EDIT] delete connection features

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   useNodesState,
   useEdgesState,
   Controls,
+  reconnectEdge,
   useReactFlow,
   Background,
 } from '@xyflow/react';
@@ -25,35 +26,64 @@ const initialNodes = [
   },
 ];
 
+// const DeleteEdgeDrop = () => {
+//   const edgeReconnectSuccessful = useRef(true);
+//   const [nodes, , onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+//   const onConnect = useCallback(
+//     (params) => setEdges((els) => addEdge(params, els)),
+//     [],
+//   );
+// }
+
 let id = 0;
 const getId = () => `dndnode_${id++}`;
 
 const DnDFlow = () => {
+  const edgeReconnectSuccessful = useRef(true);
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const { screenToFlowPosition } = useReactFlow();
   const [type] = useDnD();
-
+  
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [],
   );
-
+  
   const onDragOver = useCallback((event) => {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
   }, []);
 
+  const onReconnectStart = useCallback(() => {
+    edgeReconnectSuccessful.current = false;
+  }, []);
+  
+  const onReconnect = useCallback((oldEdge, newConnection) => {
+    edgeReconnectSuccessful.current = true;
+    setEdges((els) => reconnectEdge(oldEdge, newConnection, els));
+  }, []);
+  
+  const onReconnectEnd = useCallback((_, edge) => {
+    if (!edgeReconnectSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    }
+  
+    edgeReconnectSuccessful.current = true;
+  }, []);
+  
   const onDrop = useCallback(
     (event) => {
       event.preventDefault();
-
+      
       // check if the dropped element is valid
       if (!type) {
         return;
       }
-
+      
       // project was renamed to screenToFlowPosition
       // and you don't need to subtract the reactFlowBounds.left/top anymore
       // details: https://reactflow.dev/whats-new/2023-11-10
@@ -85,6 +115,10 @@ const DnDFlow = () => {
             onConnect={onConnect}
             onDrop={onDrop}
             onDragOver={onDragOver}
+            snapToGrid
+            onReconnect={onReconnect}
+            onReconnectStart={onReconnectStart}
+            onReconnectEnd={onReconnectEnd}
             fitView
             style={{ backgroundColor: "#F7F9FB" }}
           >
